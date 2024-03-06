3d50267d6964d495f54013365721b0d599fa7c7f
rameez <rameezf@globalkinetic.com>
2024-03-06 16:53:03 +0200

    Update UsersPage
    Update UsersTest

--- src/test/java/web/pages/users/UsersPage.java
+++ src/test/java/web/pages/users/UsersPage.java
@@ -1,6 +1,7 @@
 package web.pages.users;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
@@ -13,6 +14,9 @@ public class UsersPage extends BasePage {
     @FindBy(css = "[data-qa='search-input']")
     WebElement userSearchInputField;
 
+    @FindBy(css = "[data-qa='3-pagination-button']")
+    WebElement pageThreePaginationLink;
+
     public UsersPage(RemoteWebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
@@ -20,6 +24,12 @@ public class UsersPage extends BasePage {
 
     public void searchForUser(String username) {
         wait.until(ExpectedConditions.visibilityOf(userSearchInputField)).sendKeys(username);
+    }
+
+    public void navigateToPageThree() {
+        wait.until(ExpectedConditions.elementToBeClickable(pageThreePaginationLink));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", pageThreePaginationLink);
     }
 
     public boolean isNamePresent(String username) {
--- src/test/java/web/tests/UsersTest.java
+++ src/test/java/web/tests/UsersTest.java
@@ -34,4 +34,11 @@ public class UsersTest extends BaseTest {
         usersPage.searchForUser(NAME_IN_TABLE);
         Assert.assertTrue(usersPage.isNamePresent(NAME_IN_TABLE));
     }
+    @Test
+    @Description("As a FE user I want to test the pagination on the user table.")
+    public void testPaginationOnUserTable() {
+        usersPage.navigateToPageThree();
+        Assert.assertTrue(usersPage.isNamePresent(NAME_IN_TABLE));
+    }
+
 }
