fab4214eb8b6dad46be54488127ae3a0903962b1
rameezf <rameezf@globalkinetic.com>
2024-03-06 16:25:29 +0200

    Update README.md

--- src/test/java/web/pages/users/UsersPage.java
+++ src/test/java/web/pages/users/UsersPage.java
@@ -13,9 +13,6 @@ public class UsersPage extends BasePage {
     @FindBy(css = "[data-qa='search-input']")
     WebElement userSearchInputField;
 
-    @FindBy(css = "[data-qa='3-pagination-button']")
-    WebElement pageThreePaginationLink;
-
     public UsersPage(RemoteWebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
@@ -23,10 +20,6 @@ public class UsersPage extends BasePage {
 
     public void searchForUser(String username) {
         wait.until(ExpectedConditions.visibilityOf(userSearchInputField)).sendKeys(username);
-    }
-
-    public void navigateToPageThree() {
-        wait.until(ExpectedConditions.elementToBeClickable(pageThreePaginationLink)).click();
     }
 
     public boolean isNamePresent(String username) {
